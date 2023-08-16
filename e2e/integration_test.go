b5e8362a86c31f41d6477a296188b19c9ffd5323
Ansgar Schulte <1299623+ansgarschulte@users.noreply.github.com>
2023-08-16 16:36:44 +0200

    test: add fixAmount e2e test

--- e2e/integration_test.go
+++ e2e/integration_test.go
@@ -29,10 +29,10 @@ func TestWithMinikube(t *testing.T) {
 			Name: "periodically",
 			Test: testPeriodically,
 		},
-		//{
-		//	Name: "fixAmount",
-		//	Test: testFixAmount,
-		//},
+		{
+			Name: "fixAmount",
+			Test: testFixAmount,
+		},
 	})
 }
 
@@ -50,10 +50,98 @@ func testPeriodically(t *testing.T, m *e2e.Minikube, e *e2e.Extension) {
 		WantedFailure bool
 	}{
 		{
-			name:          "should delay no traffic",
+			name:          "should check status ok",
 			url:           "http://demo.dev.steadybit.io/products",
 			timeout:       1000,
 			WantedFailure: false,
+		},
+		{
+			name:          "should check status not ok",
+			url:           "http://demo.dev.steadybit.io/products",
+			timeout:       1,
+			WantedFailure: true,
+		},
+	}
+
+	require.NoError(t, err)
+
+	for _, tt := range tests {
+
+		config := struct {
+			Duration          int           `json:"duration"`
+			Url               string        `json:"url"`
+			ConnectTimeout    float64       `json:"connectTimeout"`
+			RequestsPerSecond float64       `json:"requestsPerSecond"`
+			Method            string        `json:"method"`
+			MaxConcurrent     float64       `json:"maxConcurrent"`
+			StatusCode        string        `json:"statusCode"`
+			ReadTimeout       float64       `json:"readTimeout"`
+			Headers           []interface{} `json:"headers"`
+		}{
+			Duration:          10000,
+			Url:               tt.url,
+			ConnectTimeout:    tt.timeout,
+			RequestsPerSecond: 2,
+			Method:            "GET",
+			MaxConcurrent:     1,
+			StatusCode:        "200",
+			ReadTimeout:       tt.timeout,
+			Headers:           []interface{}{map[string]interface{}{"key": "test", "value": "test"}},
+		}
+
+		t.Run(tt.name, func(t *testing.T) {
+			action, err := e.RunAction(exthttpcheck.TargetIDPeriodically, nil, config, nil)
+			defer func() { _ = action.Cancel() }()
+			require.NoError(t, err)
+
+			assert.Eventually(t, func() bool {
+				metrics := e.GetMetrics(exthttpcheck.TargetIDPeriodically)
+				if metrics == nil {
+					return false
+				}
+				return len(*metrics) > 2
+			}, 5*time.Second, 500*time.Millisecond)
+			metrics := e.GetMetrics(exthttpcheck.TargetIDPeriodically)
+
+			for _, metric := range *metrics {
+				if !tt.WantedFailure {
+					assert.Equal(t, "200", metric.Metric["http_status"])
+				} else {
+					assert.NotEqual(t, "200", metric.Metric["http_status"])
+					//error -> Get "http://demo.dev.steadybit.io/products": context deadline exceeded (Client.Timeout exceeded while awaiting headers)
+					assert.Contains(t, metric.Metric["error"], "context deadline exceeded")
+				}
+			}
+
+			require.NoError(t, action.Cancel())
+		})
+	}
+}
+
+func testFixAmount(t *testing.T, m *e2e.Minikube, e *e2e.Extension) {
+	log.Info().Msg("Starting testPeriodically")
+	netperf := e2e.Netperf{Minikube: m}
+	err := netperf.Deploy("delay")
+	defer func() { _ = netperf.Delete() }()
+	require.NoError(t, err)
+
+	tests := []struct {
+		name          string
+		url           string
+		timeout       float64
+		WantedFailure bool
+	}{
+		{
+			name:          "should check status ok",
+			url:           "http://demo.dev.steadybit.io/products",
+			timeout:       1000,
+			WantedFailure: false,
+		},
+		{
+			name:          "should check status not ok",
+			url:           "http://demo.dev.steadybit.io/products",
+			timeout:       1,
+			WantedFailure: true,
 		},
 	}
 
@@ -75,7 +163,7 @@ func testPeriodically(t *testing.T, m *e2e.Minikube, e *e2e.Extension) {
 			Duration:         10000,
 			Url:              tt.url,
 			ConnectTimeout:   tt.timeout,
-			NumberOfRequests: 100,
+			NumberOfRequests: 20,
 			Method:           "GET",
 			MaxConcurrent:    1,
 			StatusCode:       "200",
@@ -84,24 +172,30 @@ func testPeriodically(t *testing.T, m *e2e.Minikube, e *e2e.Extension) {
 		}
 
 		t.Run(tt.name, func(t *testing.T) {
-			action, err := e.RunAction(exthttpcheck.TargetIDPeriodically, nil, config, nil)
+			action, err := e.RunAction(exthttpcheck.TargetIDFixedAmount, nil, config, nil)
 			defer func() { _ = action.Cancel() }()
 			require.NoError(t, err)
 
 			assert.Eventually(t, func() bool {
-				metrics := e.GetMetrics(exthttpcheck.TargetIDPeriodically)
+				metrics := e.GetMetrics(exthttpcheck.TargetIDFixedAmount)
 				if metrics == nil {
 					return false
 				}
-				return len(*metrics) > 0
+				return len(*metrics) > 1
 			}, 5*time.Second, 500*time.Millisecond)
+			metrics := e.GetMetrics(exthttpcheck.TargetIDFixedAmount)
+
+			for _, metric := range *metrics {
+				if !tt.WantedFailure {
+					assert.Equal(t, metric.Metric["http_status"], "200")
+				} else {
+					assert.NotEqual(t, metric.Metric["http_status"], "200")
+					//error -> Get "http://demo.dev.steadybit.io/products": context deadline exceeded (Client.Timeout exceeded while awaiting headers)
+					assert.Contains(t, metric.Metric["error"], "context deadline exceeded")
+				}
+			}
 
 			require.NoError(t, action.Cancel())
-
 		})
 	}
-}
-
-func testFixAmount(t *testing.T, m *e2e.Minikube, e *e2e.Extension) {
-
 }
