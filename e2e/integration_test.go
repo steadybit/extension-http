08f44465192006d86764fb0bf8f611a8bcb3c7be
Johannes Edmeier <johannes.edmeier@steadybit.com>
2024-01-26 19:32:06 +0100

    text: fix wrong T used in asserts

--- e2e/integration_test.go
+++ e2e/integration_test.go
@@ -101,11 +101,11 @@ func testPeriodically(t *testing.T, m *e2e.Minikube, e *e2e.Extension) {
 						if metric.Metric["error"] != "" {
 							log.Info().Msgf("Metric error: %v", metric.Metric["error"])
 						}
-						assert.Equal(t, "200", metric.Metric["http_status"])
+						assert.Equal(c, "200", metric.Metric["http_status"])
 					} else {
-						assert.NotEqual(t, "200", metric.Metric["http_status"])
+						assert.NotEqual(c, "200", metric.Metric["http_status"])
 						//error -> Get "https://hub-dev.steadybit.com": context deadline exceeded (Client.Timeout exceeded while awaiting headers)
-						assert.Contains(t, metric.Metric["error"], "context deadline exceeded")
+						assert.Contains(c, metric.Metric["error"], "context deadline exceeded")
 					}
 				}
 			}, 5*time.Second, 500*time.Millisecond)
@@ -177,11 +177,11 @@ func testFixAmount(t *testing.T, m *e2e.Minikube, e *e2e.Extension) {
 				metrics := action.Metrics()
 				for _, metric := range metrics {
 					if !tt.WantedFailure {
-						assert.Equal(t, metric.Metric["http_status"], "200")
+						assert.Equal(c, metric.Metric["http_status"], "200")
 					} else {
-						assert.NotEqual(t, metric.Metric["http_status"], "200")
+						assert.NotEqual(c, metric.Metric["http_status"], "200")
 						//error -> Get "https://hub-dev.steadybit.com": context deadline exceeded (Client.Timeout exceeded while awaiting headers)
-						assert.Contains(t, metric.Metric["error"], "context deadline exceeded")
+						assert.Contains(c, metric.Metric["error"], "context deadline exceeded")
 					}
 				}
 			}, 5*time.Second, 500*time.Millisecond)
