136c3e49ffadfb3b21e0f4683fab8735b0c3a1f8
Daniel Reuter <daniel@steadybit.com>
2025-10-06 12:19:40 +0200

    fix: responses contain label typo

--- exthttpcheck/common.go
+++ exthttpcheck/common.go
@@ -135,7 +135,7 @@ var (
 	}
 	responsesContains = action_kit_api.ActionParameter{
 		Name:        "responsesContains",
-		Label:       "Responses contains",
+		Label:       "Responses contain",
 		Description: extutil.Ptr("The responses must contain the given string, otherwise the step will fail."),
 		Type:        action_kit_api.ActionParameterTypeString,
 		Required:    extutil.Ptr(false),
